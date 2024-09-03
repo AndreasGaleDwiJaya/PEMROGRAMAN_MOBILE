85533081ae81418ece4b4488e1cddf1520afa06f
Andreas Gale Dwi Jaya <andreasgaledwijaya@gmail.com>
2024-09-03 12:19:00 +0700

    Pertemuan2

--- bin/tugas2.dart
+++ bin/tugas2.dart
@@ -1,5 +1,5 @@
 void main() {
-  for (int i = 18; i < 8; i++) {
-    print('Nama saya adalah Fulan, sekarang berumur ${i - 1}');
+  for (int i = 18; i > 8; i--) {
+    print('Nama saya adalah Fulan, sekarang berumur $i');
   }
 }
